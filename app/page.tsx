d8497bb7d7225db9091b5fc0200fb008eb1ffb82
apooravm <medal.apoorav@gmail.com>
2025-04-07 23:38:09 +0100

    more

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
 
 export default function Home() {
     return (
-        <div className="flex flex-col justify-center items-center text-red-500">
-            <span>DETECT PHISHING LINKS NOW!!!</span>
-            <div className="flex flex-col gap-4 w-full justify-center items-center">
+        <div className="flex flex-col justify-between text-red-500 h-screen">
+            <div className="flex flex-col gap-4 justify-center items-center">
+                <span>DETECT PHISHING LINKS NOW!!!</span>
                 <span>
-                    Sketchy links in your inbox? Tired of clicking on links that lead to nowhere?
+                    Sketchy links in your inbox? Tired of clicking on links that
+                    lead to nowhere?
                 </span>
                 <span>Detect them now!</span>
-                <span>Possible through our best in class phishing detection technology.</span>
                 <span>
-                     (No legal advice provided. But we *will* catch those shady links.)
+                    Possible through our best in class phishing detection
+                    technology.
+                </span>
+            </div>
+            <div className="self-center text-xs py-2">
+                <span>
+                    (No legal advice provided. But we *will* catch those shady
+                    links.)
                 </span>
             </div>
         </div>
